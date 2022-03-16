3e43e96199814d30b4ff705081eeba2d089419de
“mdreu” <mdreusi@gmail.com>
2022-03-16 09:50:24 -0400

    Hw

---
+++ script.js
@@ -0,0 +1,17 @@
+for(i = 1; i<=7; i++) {
+    console.log(i);
+}
+for(i = 5; i <= 25; i ++) {
+    console.log(i);
+}
+const wizards = ['Harry Potter', 'Hermione Granger', 'Ron Weasley'];
+
+for(list of wizards) {
+    console.log(list);
+}
+let harryPotterMovies = 0;
+while(harryPotterMovies < 8) {
+    harryPotterMovies ++;
+    
+}
+console.log(harryPotterMovies);
